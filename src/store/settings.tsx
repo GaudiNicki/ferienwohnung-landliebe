cbda2c0e30d3f5d811a1c596c7288d871d32f650
tensorwitz <niklas.witzel@tensora.co>
2025-04-25 20:57:14 +0200

    Update blocked periods

--- src/store/settings.tsx
+++ src/store/settings.tsx
@@ -24,6 +24,60 @@ export const useSettingsStore = create<SettingsStore>()(
           endDate: '2025-04-27',
           reason: 'Nicole Martina Hennich'
         },
+        {
+          id: '2',
+          startDate: '2025-05-05',
+          endDate: '2025-05-13',
+          reason: 'Hochzeit Niklas'
+        },
+        {
+          id: '3',
+          startDate: '2025-05-15',
+          endDate: '2025-05-18',
+          reason: 'Martin Zimmer'
+        },
+        {
+          id: '4',
+          startDate: '2025-06-06',
+          endDate: '2025-06-09',
+          reason: 'Gerrit Stöver'
+        },
+        {
+          id: '5',
+          startDate: '2025-06-10',
+          endDate: '2025-06-23',
+          reason: 'Urlaub'
+        },
+        {
+          id: '6',
+          startDate: '2025-07-24',
+          endDate: '2025-07-27',
+          reason: 'Fam. Weniger'
+        },
+        {
+          id: '7',
+          startDate: '2025-08-15',
+          endDate: '2025-08-18',
+          reason: 'Annett Richterin'
+        },
+        {
+          id: '8',
+          startDate: '2025-08-21',
+          endDate: '2025-08-24',
+          reason: 'Ute Hölscher'
+        },
+        {
+          id: '9',
+          startDate: '2025-09-08',
+          endDate: '2025-09-15',
+          reason: 'Urlaub'
+        },
+        {
+          id: '10',
+          startDate: '2025-10-03',
+          endDate: '2025-10-06',
+          reason: 'Saskia van de Post'
+        },
       ],
       setPrices: (prices) => set({ prices }),
       setBlockedPeriods: (blockedPeriods) => set({ blockedPeriods })
